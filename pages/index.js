698fa790f06d90cd3b63392ac4f898bf35b860e5
Murray Williams <murray@waterfalldigital.co.za>
2021-10-28 20:25:49 +0200

    Event Item page and limited home events to 3

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
+import EventItem from '@/components/EventItem'
 
 export default function Home({ events }) {
   console.log(events)
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   )
 }
@@ -15,7 +21,7 @@ export async function getStaticProps() {
   const events = await res.json()
 
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1,
   }
 }
