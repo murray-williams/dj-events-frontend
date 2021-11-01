059c0e804e8467e43dc2a533db5678d038f656f7
Murray Williams <murray@waterfalldigital.co.za>
2021-11-01 15:51:36 +0200

    Built event details page

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,7 +3,6 @@ import { API_URL } from '@/config/index'
 import EventItem from '@/components/EventItem'
 
 export default function EventsPage({ events }) {
-  console.log(events)
   return (
     <Layout>
       <h1>Events</h1>
